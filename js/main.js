b09a4fb13d8b09254bc02d9c0624c66965821bea
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-11-13 09:19:23 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(function() {
-  var template = $('#template').html();
-  Mustache.parse(template);
-  var rendered = Mustache.render(template, {talks: data});
-  $('.article-headline').html(rendered);
+$(function () {
+    var template = $('#template').html();
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, { talks: data });
+    $('.article-headline').html(rendered);
 
-  var hash_temp = location.hash;
-  if (hash_temp) {
-    location.replace('#');
-    location.replace(hash_temp);
-  }
+    var hash_temp = location.hash;
+    if (hash_temp) {
+        location.replace('#');
+        location.replace(hash_temp);
+    }
 });
